1f528f11d007986a4395215a8717909b7b464647
Sean Stanway <sean.stanway@hotmail.co.uk>
2018-09-07 11:09:12 +0100

    Added function and If statement tutorial code

--- python-training-vs/python_training_vs.py
+++ python-training-vs/python_training_vs.py
@@ -2,3 +2,19 @@ def print_function(exampleName, content):
     print('{} {}'.format(exampleName, content))
 def newLine():
     print('')
+
+def my_function():
+    a = 2
+    return a
+print_function("Function:", my_function())
+
+newLine()
+
+b = 5
+c = 7
+if b > c:
+    print_function('If Statement:', 'b is more than c')
+else:
+    print_function('If Statement:','c is more than b')
+
+newLine()
